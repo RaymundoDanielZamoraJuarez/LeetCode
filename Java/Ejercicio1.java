82c8c95b4aa62c66755c4e4564debdb0b996cbcb
kristian.hernandez@encora.com <kristian.hernandez@ENCEDCREMLT0781.local>
2025-05-08 20:30:18 -0600

    Added instructions to Java Problem1

--- Java/Ejercicio1.java
+++ Java/Ejercicio1.java
@@ -1,3 +1,15 @@
+// Median of Two Sorted Arrays
+// Given two sorted arrays nums1 and nums2 of size m and n respectively,
+// return the median of the two sorted arrays.
+
+// Example 1:
+// Input: nums1 = [1, 3], nums2 = [2]
+// Output: 2.00000
+
+// Example 2:
+// Input: nums1 = [1, 2], nums2 = [3, 4]
+// Output: 2.50000
+
 import java.util.Arrays;
 
 class Solution {
