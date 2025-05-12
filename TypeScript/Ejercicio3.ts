a77690a1f8ceb05f681a92dbd6a509c3467b80b9
LeopoldoDelgadillo <leopoldo.d@encora.com>
2025-05-11 22:50:44 -0600

    Optimization Ejercicio3.ts

--- TypeScript/Ejercicio3.ts
+++ TypeScript/Ejercicio3.ts
@@ -6,25 +6,15 @@ function lengthOfLongestSubstring(s: string): number {
   if (s.length != 1) {
     for (var i = 0; i < s.length; i++) {
       subString.push(s.charAt(i));
-      var j = subString.lastIndexOf(s.charAt(i)) - 1;
-      while (j != -1 && subString.length > 1) {
-        if (subString[j] == s.charAt(i)) {
-            if (subLen > highLen) {
-                highLen = subLen;
-            }
-            var dupeIndex = subString.indexOf(subString[subLen]);
-            if (j < 1) {
-                subString.shift();
-                subLen--;
-            } else {
-            for (var k = 0; k <= dupeIndex; k++) {
-              subString.shift();
-              subLen--;
-            }
-            break;
-          }
+      if(subString.length > 1 && subString.indexOf(subString[subLen]) != subLen) {
+        if (subLen > highLen) {
+            highLen = subLen;
         }
-        j=j-1;
+        var dupeIndex = subString.indexOf(subString[subLen]);
+        for (var k = 0; k <= dupeIndex; k++) {
+            subString.shift();
+            subLen--;
+        }
       }
       subLen++;
     }
